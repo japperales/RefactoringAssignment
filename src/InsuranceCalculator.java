55d885b04f3f9b68ace2d373d42c9771e5d6ac6e
John <japperales@gmial.com>
2020-02-14 16:00:18 -0500

    Cleanup

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -5,43 +5,16 @@ public class InsuranceCalculator {
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             strategy = new InsuranceStrategyVeryLow();
-            return strategy.calculateInsuranceVeryHigh(income);
+            return strategy.calculate(income);
         } else if (income <= 30000) {
             strategy = new InsuranceStrategyLow();
-            return strategy.calculateInsuranceVeryHigh(income);
+            return strategy.calculate(income);
         } else if (income <= 60000) {
             strategy = new InsuranceStrategyHigh();
-            return strategy.calculateInsuranceVeryHigh(income);
+            return strategy.calculate(income);
         } else {
             strategy = new InsuranceStrategyVeryHigh();
-            return strategy.calculateInsuranceVeryHigh(income);
+            return strategy.calculate(income);
         }
     }
-
-    private double calculcateInsuranceHigh(double income, int i, double v, int i2) {
-        return (income - i) * v + i2;
-    }
-
-    private double calculateInsuranceLow(double income, int i, double v, int i2) {
-        return (income - i) * v + i2;
-    }
-
-    private double calculateInsuranceVeryLow(double income, double v) {
-        return income * v;
-    }
-
-    private double CalculateInsuranceVeryHigh(double income) {
-        return strategy.calculateInsuranceVeryHigh(income);
-    }
-    public int getConstant() {
-        return strategy.getConstant();
-    }
-
-    public double getWeight() {
-        return strategy.getWeight();
-    }
-    public int getAdjustment() {
-        return strategy.getAdjustment();
-    }
-
 }
